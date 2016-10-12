325d402ad9e67df1e923077674398a8c6fbb6625
YJ Park <yjpark@gmail.com>
2016-10-12 16:12:06 +0800

    add support for projitems

--- silp/language.py
+++ silp/language.py
@@ -22,4 +22,5 @@ languages = [
     Language('Erlang Include', '.hrl', '%SILP:', '%__SILP__\n', 80),
     Language('SQL', '.sql', '-- SILP:', '/*__SILP__*/\n', 80),
     Language('CS Proj', '.csproj', '<!--SILP:', '<!--__SILP__-->\n', 80),
+    Language('Proj Items', '.projitems', '<!--SILP:', '<!--__SILP__-->\n', 80),
 ]
